ad195237e1d8d6a0d77b306ee2fcc2b8af981a44
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2018-08-31 15:20:07 +0200

    added & refactored TilemapController

---
+++ Source/Code/Companion/MapGen/TilemapController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Duality.Editor;
+using Duality.Plugins.Tilemaps;
+
+namespace Duality.Plugins.Companion.MapGen
+{
+    [EditorHintCategory(ResNames.MapGenCategory)]
+    [RequiredComponent(typeof(Tilemap))]
+    public class TilemapController : Component
+    {
+        private int _emptyTileIndex;
+        private int _solidTileIndex;
+        [DontSerialize] private Tilemap _tilemap;
+
+        public int EmptyTileIndex1
+        {
+            get => _emptyTileIndex;
+            set => _emptyTileIndex = value;
+        }
+
+        public int SolidTileIndex
+        {
+            get => _solidTileIndex;
+            set => _solidTileIndex = value;
+        }
+
+        public void SetupTilemap(MapGenerationData genData)
+        {
+            _tilemap = GameObj.GetComponent<Tilemap>();
+            _tilemap.Resize(genData.Width, genData.Height);
+            var tileGrid = _tilemap.BeginUpdateTiles();
+            tileGrid.Fill(new Tile(_emptyTileIndex), 0, 0, tileGrid.Width, tileGrid.Height);
+            _tilemap.EndUpdateTiles();
+        }
+
+        public void UpdateTilemap(Grid<TileType> generatedMap, int xMin, int xMax, int yMin, int yMax)
+        {
+            var tileGrid = _tilemap.BeginUpdateTiles();
+
+            for (var y = yMin; y <= yMax; y++)
+            {
+                for (var x = xMin; x <= xMax; x++)
+                {
+                    int tileIndex;
+                    switch (generatedMap[x, y])
+                    {
+                        case TileType.Solid:
+                            tileIndex = _solidTileIndex;
+                            break;
+                        case TileType.Empty:
+                            tileIndex = _emptyTileIndex;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    tileGrid[x, y] = new Tile(tileIndex);
+                }
+            }
+            _tilemap.EndUpdateTiles();
+        }
+
+        public void UpdateTilemap(Grid<TileType> generatedMap)
+        {
+            UpdateTilemap(generatedMap, 0, generatedMap.Width - 1, 0, generatedMap.Height - 1);
+        }
+    }
+}
--- Source/Code/Companion/ResNames.cs
+++ Source/Code/Companion/ResNames.cs
@@ -5,5 +5,6 @@
 		public const string ComponentsEditorCategory = "Companion/Components";
 		public const string DrawingEditorCategory = "Companion/Drawing";
 		public const string DiagnosticsEditorCategory = "Companion/Diagnostics";
+		public const string MapGenCategory = "Companion/MapGen";
 	}
 }
